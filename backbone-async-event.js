8e9201a25a310076e07baa2cd0d7d26ded2a3832
Joe Hudson <joehud@gmail.com>
2014-05-15 08:06:33 -0400

    fix author email address

--- backbone-async-event.js
+++ backbone-async-event.js
@@ -1,7 +1,8 @@
 /*!
  * backbone-async-event v0.1.3
+ * https://github.com/jhudson8/backbone-async-event
  * 
- * Copyright (c) 2014 Joe Hudson
+ * Copyright (c) 2014 Joe Hudson<joehud_AT_gmail.com>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
